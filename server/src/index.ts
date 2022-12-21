b2fafb3f786f552c60c1276a0f99e7adee0ab7d0
LilMartin <martin_du@qq.com>
2022-12-21 21:31:03 +0800

    init: 服务端初始化

---
+++ server/src/index.ts
@@ -0,0 +1,5 @@
+import express from 'express'
+const app = express()
+const port = 5000
+app.get('/', (_, res) => { res.status(200).send('aadddd') })
+app.listen(port, () => console.log(`Running on port ${port}`))  
